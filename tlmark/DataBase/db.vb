69f05faec69df9433264a5e4ad32d8d1de1ed260
CRISTIAN CAMILO ROMERO CARDENAS <ccromero@utp.edu.co>
2019-04-25 21:07:32 -0500

    Inicio de proyecto

---
+++ tlmark/DataBase/db.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+
+
+
+Public Class Db
+
+    Dim con As New SqlConnection(ConfigurationManager.ConnectionStrings("con").ConnectionString)
+    Dim cmd As New SqlCommand()
+
+
+
+
+
+End Class
